898526ea8e5031276273771104fcd6a69c5c1a47
sos1819mdls <lolimarilopez@hotmail.es>
2019-03-19 16:57:00 +0000

    todo

--- index.js
+++ index.js
@@ -423,10 +423,5 @@ app.listen(port, () => {
 
 app.use("/", express.static(__dirname + "/public"));
 
-app.get("/time", (request, response) => {
-    response.send(new Date());
 
-});
-app.listen(port, () => {
-    console.log("Magic is happening in port " + port);
-});
+
