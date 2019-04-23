8fdc8d8c999209d0514a19723a3506fefce325c9
sos1819fgm <felgonmar@alum.us.es>
2019-04-23 08:37:21 +0000

    Probando nuevos

--- public/expenses/expenses-ctrl.js
+++ public/expenses/expenses-ctrl.js
@@ -3,8 +3,19 @@
  angular.module("minipostmanAPP").controller("MainCtrl", ["$scope", "$http", function($scope, $http){
             console.log("MainCtrl initialized");
             $scope.url = "/api/v1/expenses-of-countries-in-education-and-culture";
-            
+            var API = "https://sos1819-08.herokuapp.com/api/v1/expenses-of-countries-in-education-and-culture";
             var path="https://sos1819-08.herokuapp.com"
+            function refresh(){
+                console.log("Requesting expenses to <"+API+">");
+                $http.get(API).then(function(response) {
+                    console.log("Data received: "+ JSON.stringify(response.data));
+                    $scope.expenses = response.data;
+                })
+            }
+            
+
+        
+            
             $scope.sendGet = function (){
                 $http.get(path+$scope.url).then(function(response){
                 $scope.data = JSON.stringify(response.data, null,2);
@@ -112,6 +123,7 @@
              $scope.clear = function(){
                  $scope.data="";
              };
+             
              
             
        } ] );
