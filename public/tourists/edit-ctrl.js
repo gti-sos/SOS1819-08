0588cb31db932838937b85e1dc56aabd69c1bccc
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-09 22:56:56 +0000

    editar

--- public/tourists/edit-ctrl.js
+++ public/tourists/edit-ctrl.js
@@ -6,7 +6,7 @@
                    console.log("Edit Tourists Initialized!");
 
 
-                   var API = "https://sos1819-08.herokuapp.com/api/v1/tourists-by-countries/#!/"+$routeParams.province+"/"+$routeParams.year;
+                   var API = "https://sos1819-08.herokuapp.com/api/v1/tourists-by-countries/"+$routeParams.province+"/"+$routeParams.year;
                    var path = "https://sos1819-08.herokuapp.com"
                    refresh();
 
