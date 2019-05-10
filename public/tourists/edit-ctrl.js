bd73fc33669a358b974ffbbec30e793b4bd5582d
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-10 12:04:27 +0000

    editar

--- public/tourists/edit-ctrl.js
+++ public/tourists/edit-ctrl.js
@@ -9,74 +9,26 @@ angular
         var API = "/api/v1/tourists-by-countries/" + $routeParams.country + "/" + $routeParams.year;
 
 
-        function refresh() {
-            console.log("Requesting contacts to <" + API + ">...");
-            $http.get(API + "?limit=" + 10 + "&offset=" + pag).then(function(response) {
-                console.log("Dato recivido: " + JSON.stringify(response.data));
-                $scope.touristsByCountries = response.data;
-
-            });
-        };
-
         ///La variable put se pone a true para señalar que lo que vamos a hacer es actualizar la base de datos, es decir, hacer un put    
         $http.get(API).then(function(response) {
             $scope.updateTourist = response.data;
         });
 
-        $scope.updateTourist = function(country, year, touristDeparture, arrivalTourist, incomeTourist) {
-            if (typeof country !== 'undefined' &&
-                typeof year !== 'undefined' &&
-                typeof touristDeparture !== 'undefined' &&
-                typeof arrivalTourist !== 'undefined' &&
-                typeof incomeTourist !== 'undefined') {
-                var data = {
-                    country: country,
-                    year: parseInt(year),
-                    touristDeparture: parseInt(touristDeparture),
-                    arrivalTourist: parseInt(arrivalTourist),
-                    incomeTourist: parseInt(incomeTourist)
-                };
 
-
-                console.log("Este es el nuevo dato:  " + data);
-                $http.put(API + "/" + country + "/" + year, JSON.stringify(data)).then(function(response) {
-                    console.log("put done");
-                    $scope.dataResponse = " Código: " + response.status + "\n" + response.statusText + " Dato modificado";
-                    refresh();
-                }, function(response) {
-                    console.log("Error método PUT: Código" + response.status + ", " + response.statusText);
-                    $scope.dataResponse = "Código: " + response.status + "\n" + response.statusText + "Dato no ha sido modificado";
-                    refresh();
-                });
-            }
-            else {
-                $scope.dataResponse = "Datos incompletos";
-            }
-
-
-
+        $scope.updateTourist = function() {
+            $http.put(API, $scope.updateTourist).then(function(response) {
+                $scope.status = "Status: " + response.status;
+                
+                window.alert("OK: estadistica actualizada");
+                $location.path("/tourists-by-countries");
+            }, function() {
+                if ($scope.updateTourist["touristDeparture"]== null ||
+                    $scope.updateTourist["arrivalTourist"] == null ||
+                    $scope.updateTourist["incomeTourist"] == null) {
+                    $scope.status = "Error: debe completar todos los campos"
+                }
+            });
         };
-
-
-
-        // $http.get(API).then(function (response){
-        //     $scope.sendPut= response.data;
-        // });
-
-        // $scope.updateTourist = function() {
-        //     $http.put(API, $scope.updateTourist).then(function(response) {
-        //         $scope.status = "Status: " + response.status;
-        //         //console.log(Object.keys($scope.updatedMedicalAttentionRate).length)
-        //         window.alert("OK: estadistica actualizada");
-        //         $location.path("/tourists-by-countries/");
-        //     }, function() {
-        //         if ($scope.updateTourist["touristDeparture"]== null ||
-        //             $scope.updateTourist["arrivalTourist"] == null ||
-        //             $scope.updateTourist["incomeTourist"] == null) {
-        //             $scope.status = "Error: debe completar todos los campos"
-        //         }
-        //     });
-        // };
 
 
 
