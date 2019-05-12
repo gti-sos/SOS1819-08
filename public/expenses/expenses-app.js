9babb5ac21913b8e35bff11456d1373abb251d91
sos1819fgm <felgonmar@alum.us.es>
2019-05-12 16:43:45 +0000

    trying new files

--- public/expenses/expenses-app.js
+++ public/expenses/expenses-app.js
@@ -2,7 +2,7 @@
 
 angular.module("minipostmanAPP", ["ngRoute"])
 .config(function ($routeProvider){
-    $routeProvider.when("/",{
+    $routeProvider.when("/ui/v1/expenses-of-countries-in-education-and-culture",{
         controller : "ListCtrl",
         templateUrl: "list-expenses.html"
     })
