f8373ed57a8ae9629fff4b86f4832e6eed73017c
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-13 13:31:51 +0000

    intentos test

--- tests/e2e/TC02-newTourist.js
+++ tests/e2e/TC02-newTourist.js
@@ -1,3 +1,23 @@
+describe("Check if a new sport center can be created",function () {
+    it("List should grow after the contact creation", function (){
+        browser.get("localhost:8080/ui/v1/tourists-by-countries/#!/");
+        
+                element(by.model('id')).sendKeys('22');
+                element(by.model('country')).sendKeys('Tarfia');
+                element(by.model('year')).sendKeys('2000');
+                element(by.model('touristDeparture')).sendKeys('41012');
+                element(by.model('arrivalTourist')).sendKeys('1989');
+                element(by.model('incomeTourist')).sendKeys('4000');
+
+                element(by.css(".boton-crear")).click();
+                
+                var resultModal = element(by.css(".alert-success"));
+                expect(resultModal.getText()).toContain('Creado correctamente');
+
+            });
+    });
+
+
 describe('Add Stadistic', function() {
     it('should add a new medical Attention Rate', function() {
         browser
--- tests/tests.js
+++ tests/tests.js
@@ -1,19 +1,14 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	chromeOnly: true,
+	    capabilities: {
+        'browserName': 'phantomjs',
+    },
+
 	specs: [
 		"e2e/TC01-loadDataTourist.js",
 		"e2e/TC02-newTourist.js"
 //		"e2e/TC03-deleteTourist.js"
 	]
-	// capabilities:{
-	// 	'browserName': 'phantomjs'
-	// },
-	// params:{
-	// 	host:'localhost',
-	// 	port: '8080'
-	// }
 };
-// export.getURL = function(){
-// 	return "http://"+browser.params.host+":"+browser.params.port;
 
