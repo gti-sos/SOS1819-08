d1ec3ed3021a978aadf2ccb689d62bf22ff2494e
sos1819cjn <crisjinu@hotmail.com>
2019-05-13 11:10:40 +0000

    f

--- public/emigration/emigrationapp/edit-ctrl.js
+++ public/emigration/emigrationapp/edit-ctrl.js
@@ -8,7 +8,7 @@ angular
         
         $http.get(API+"/"+country+"/"+year).then(function (response){
                 console.log("Data Recieved: "+ JSON.stringify(response.data,null,2));            
-                $scope.updateCountry = response.data;
+                $scope.updatedCountry = response.data;
             
             });
         
@@ -16,7 +16,7 @@ angular
             console.log("Updating a new country");
             
             
-            $http.put(API+"/"+country+"/"+year, $scope.updateCountry).then(function (response){
+            $http.put(API+"/"+country+"/"+year, $scope.updatedCountry).then(function (response){
                 $scope.status= "Status: Registro añadido con éxito";
                 console.log("POST Response "+ response.status + "" + response.data);            
                
