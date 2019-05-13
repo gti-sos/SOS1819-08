707f052ade757495965b127b79fe53748b32feab
sos1819fgm <felgonmar@alum.us.es>
2019-05-13 18:11:23 +0000

    test add

---
+++ tests/e2e/TC03-deleteExpense.js
@@ -0,0 +1,20 @@
+describe('Checking if an expense is deleted', function(){
+    it('should show some expenses',function(){
+        browser
+            .get('https://sos1819-08.herokuapp.com/ui/v1/expenses-of-countries-in-education-and-culture/#!/');
+            
+             element
+                .all(by.repeater("expense in expenses"))
+                .then(function(initialExpenses){
+                     console.log(initialExpenses.length);
+                      browser.driver.sleep(2000);
+                      element.all(by.css('[value="delete"]')).last().click();
+                      
+                      element.all(by.repeater("expense in expenses"))
+                .then(function(finalExpenses){
+                 expect(finalExpenses.length).toEqual(initialExpenses.length-1);
+                    
+                    
+                    
+                })
+                })})})
