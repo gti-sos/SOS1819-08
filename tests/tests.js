3290c641a005c21b5f716defefcf58104ebf6dd8
sos1819fgm <felgonmar@alum.us.es>
2019-05-13 18:12:09 +0000

    test add

--- tests/tests.js
+++ tests/tests.js
@@ -10,7 +10,8 @@ exports.config = {
 	//	"e2e/TC02-newTourist.js",
 	//	"e2e/TC03-deleteTourist.js"
 	"e2e/TC01-loadDataExpenses.js",
-	"e2e/TC02-newExpense.js"
+	"e2e/TC02-newExpense.js",
+	"e2e/TC03-deleteExpense.js"
 	]
 };
 
