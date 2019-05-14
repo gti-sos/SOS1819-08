643488add68835cc5ea3cc8c10ffc17521275dad
sos1819fgm <felgonmar@alum.us.es>
2019-05-14 16:25:00 +0000

    me

--- tests/e2e/TC01-loadDataExpenses.js
+++ tests/e2e/TC01-loadDataExpenses.js
@@ -1,7 +1,7 @@
 describe('expenses is Loaded', function(){
     it('should show some expenses',function(){
         browser
-            .get('https://sos1819-08.herokuapp.com/ui/v1/expenses-of-countries-in-education-and-culture/#!/')
+            .get('https://sos1819-08.herokuapp.com/#!/expenses-of-countries-in-education-and-culture')
             .then(function(){
                  browser.driver.sleep(2000);
                 element(by.css('[value="cargaDatos"]')).click();
