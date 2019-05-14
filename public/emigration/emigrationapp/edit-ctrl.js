dbd7ef7f9590a02f6f6314b716430898facebf82
sos1819cjn <crisjinu@hotmail.com>
2019-05-14 16:08:49 +0000

    a

--- public/emigration/emigrationapp/edit-ctrl.js
+++ public/emigration/emigrationapp/edit-ctrl.js
@@ -19,7 +19,7 @@ angular
             $http.put(API+"/"+country+"/"+year, $scope.updatedCountry).then(function (response){
                 $scope.status= "Status: Registro modificado con éxito";
                 console.log("POST Response "+ response.status + "" + response.data);            
-               $location.path("/#!/emigrations-by-countries/");
+               $location.path("/#!/emigrations-by-countries");
             
             }, function() {
                 if ($scope.updateCountry.emigrantman== null ||
