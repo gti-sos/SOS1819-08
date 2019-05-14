6ab3b17224c8611b98e361207cf098ef0ee23564
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-14 16:11:17 +0000

    arreglos ml

--- public/tourists/edit-ctrl.js
+++ public/tourists/edit-ctrl.js
@@ -6,7 +6,7 @@ angular
         console.log("Edit Tourists Initialized!");
 
 
-        var API = "/tourists-by-countries/" + $routeParams.country + "/" + $routeParams.year;
+        var API = "api/v1/tourists-by-countries/" + $routeParams.country + "/" + $routeParams.year;
 
 
      
