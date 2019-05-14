327cabe40b711007766aff2918c01b9ccfe8bd78
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-14 10:17:52 +0000

    values ml

--- tests/tests.js
+++ tests/tests.js
@@ -7,15 +7,15 @@ exports.config = {
 
 	specs: [
 
-		"e2e/TC01-loadDataTourist.js",
-		"e2e/TC02-newTourist.js",
+		// "e2e/TC01-loadDataTourist.js",
+		// "e2e/TC02-newTourist.js",
 		"e2e/TC03-deleteTourist.js",
-		"e2e/TC01-loadDataExpenses.js",
-		"e2e/TC02-newExpense.js",
-		"e2e/TC03-deleteExpense.js",
-		"e2e/TC01-loadDataEmigration.js",
-		"e2e/TC02-newEmigration.js",
-		"e2e/TC03-deleteEmigration.js"
+		// "e2e/TC01-loadDataExpenses.js",
+		// "e2e/TC02-newExpense.js",
+		// "e2e/TC03-deleteExpense.js",
+		// "e2e/TC01-loadDataEmigration.js",
+		// "e2e/TC02-newEmigration.js",
+		// "e2e/TC03-deleteEmigration.js"
 	]
 };
 
