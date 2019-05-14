139174e7b611f8f06e61561069e01bcef8bb1fb5
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-14 15:55:54 +0000

    arreglos ml

--- public/global-app.js
+++ public/global-app.js
@@ -1,29 +1,39 @@
 /*global angular*/
 
 angular.module("app", ["ngRoute"])
-.config(function ($routeProvider){
-    $routeProvider.when("/",{
-        templateUrl: "info.html"
-    }).when("/expenses-of-countries-in-education-and-culture",{
-        controller : "ListCtrl-expenses",
-        templateUrl: "expenses/list-expenses.html"
-    })
-    
-    
-    .when("/expenses-of-countries-in-education-and-culture/:country/:year",{
-         controller : "EditCtrl",
-        templateUrl: "expenses/edit-expenses.html"
-    })
-    
-    .when("/emigrations-by-countries",{
-        controller : "ListCtrlEmigration",
-        templateUrl: "emigration/emigrationapp/list.html"
-    })
-    
-    .when("/emigrations-by-countries/:country/:year",{
-    controller : "EditCtrlEmigration",
-    templateUrl: "emigration/emigrationapp/edit.html"
-            });
-    
-});
-        console.log("angular app ok");
+    .config(function($routeProvider) {
+        $routeProvider.when("/", {
+                templateUrl: "info.html"
+            }).when("/expenses-of-countries-in-education-and-culture", {
+                controller: "ListCtrl-expenses",
+                templateUrl: "expenses/list-expenses.html"
+            })
+
+
+            .when("/expenses-of-countries-in-education-and-culture/:country/:year", {
+                controller: "EditCtrl",
+                templateUrl: "expenses/edit-expenses.html"
+            })
+
+            .when("/emigrations-by-countries", {
+                controller: "ListCtrlEmigration",
+                templateUrl: "emigration/emigrationapp/list.html"
+            })
+
+            .when("/emigrations-by-countries/:country/:year", {
+                controller: "EditCtrlEmigration",
+                templateUrl: "emigration/emigrationapp/edit.html"
+            })
+
+        .when("/tourists-by-countries", {
+            controller: "ListCtrl",
+            templateUrl: "tourist/list.html"
+        })
+        .when("/tourists-by-countries/:country/:year", {
+            controller: "EditCtrl",
+            templateUrl: "tourist/edit.html"
+        });
+
+
+    });
+console.log("angular app ok");
