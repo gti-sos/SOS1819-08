0b2d29b6d47c499b0bfd549a213f9d8b18178004
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-30 18:41:13 +0000

    cors añadido

--- public/tourists/analytics/integracionG08.1.js
+++ public/tourists/analytics/integracionG08.1.js
@@ -1,111 +1,111 @@
 angular
     .module("app")
     .controller("integracionG08", ["$scope", "$http",
-            function($scope, $http) {
-                console.log("integracionG08!");
+        function($scope, $http) {
+            console.log("integracionG08!");
 
-                var API = "api/v1/tourists-by-countries";
-                var API2 = "api/v1/emigrations-by-countries";
+            var API = "api/v1/tourists-by-countries";
+            var API2 = "api/v1/emigrations-by-countries";
 
 
 
-                $http.get(API).then(function(response) {
-                    $http.get(API2).then(function(response1) {
-                        
-                        Highcharts.chart('container', {
-    chart: {
-        type: 'packedbubble',
-        height: '100%'
-    },
-    title: {
-        text: 'Tourists'
-    },
-    tooltip: {
-        useHTML: true,
-        pointFormat: '<b>{point.name}:</b> {point.value}m CO<sub>2</sub>'
-    },
-    plotOptions: {
-        packedbubble: {
-            minSize: '20%',
-            maxSize: '100%',
-            zMin: 0,
-            zMax: 1000,
-            layoutAlgorithm: {
-                gravitationalConstant: 0.05,
-                splitSeries: true,
-                seriesInteraction: false,
-                dragBetweenSeries: true,
-                parentNodeLimit: true
-            },
-            dataLabels: {
-                enabled: true,
-                format: '{point.name}',
-                filter: {
-                    property: 'y',
-                    operator: '>',
-                    value: 250
-                },
-                style: {
-                    color: 'black',
-                    textOutline: 'none',
-                    fontWeight: 'normal'
-                }
-            }
+            $http.get(API).then(function(response) {
+                $http.get(API2).then(function(response1) {
+
+                    Highcharts.chart('container', {
+                        chart: {
+                            type: 'packedbubble',
+                            height: '100%'
+                        },
+                        title: {
+                            text: 'Tourists'
+                        },
+                        tooltip: {
+                            useHTML: true,
+                            pointFormat: '<b>{point.name}:</b> {point.value}m CO<sub>2</sub>'
+                        },
+                        plotOptions: {
+                            packedbubble: {
+                                minSize: '20%',
+                                maxSize: '100%',
+                                zMin: 0,
+                                zMax: 1000,
+                                layoutAlgorithm: {
+                                    gravitationalConstant: 0.05,
+                                    splitSeries: true,
+                                    seriesInteraction: false,
+                                    dragBetweenSeries: true,
+                                    parentNodeLimit: true
+                                },
+                                dataLabels: {
+                                    enabled: true,
+                                    format: '{point.name}',
+                                    filter: {
+                                        property: 'y',
+                                        operator: '>',
+                                        value: 250
+                                    },
+                                    style: {
+                                        color: 'black',
+                                        textOutline: 'none',
+                                        fontWeight: 'normal'
+                                    }
+                                }
+                            }
+                        },
+                        series: [{
+                            name: 'Tourists Departure',
+                            data: [parseInt(response.data.filter(d => d.country == 'Spain').map(function(d) { return d["touristDeparture"] })),
+                                parseInt(response.data.filter(d => d.country == 'China').map(function(d) { return d["touristDeparture"] })),
+                                parseInt(response.data.filter(d => d.country == 'Colombia').map(function(d) { return d["touristDeparture"] })),
+                                parseInt(response.data.filter(d => d.country == 'Germany').map(function(d) { return d["touristDeparture"] })),
+                                parseInt(response.data.filter(d => d.country == 'USA').map(function(d) { return d["touristDeparture"] }))
+
+                            ]
+                        }, {
+                            name: 'Income Tourist',
+                            data: [parseInt(response.data.filter(d => d.country == 'Spain').map(function(d) { return d['incomeTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'China').map(function(d) { return d['incomeTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'Colombia').map(function(d) { return d['incomeTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'Germany').map(function(d) { return d['incomeTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'USA').map(function(d) { return d['incomeTourist'] }))
+                            ]
+                        }, {
+                            name: 'arrival Tourist',
+                            data: [parseInt(response.data.filter(d => d.country == 'Spain').map(function(d) { return d['arrivalTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'China').map(function(d) { return d['arrivalTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'Colombia').map(function(d) { return d['arrivalTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'Germany').map(function(d) { return d['arrivalTourist'] })),
+                                parseInt(response.data.filter(d => d.country == 'USA').map(function(d) { return d['arrivalTourist'] }))
+                            ]
+                        }, {
+                            name: 'total emigrations',
+                            data: [parseInt(response1.data.filter(d => d.country == 'Spain').map(function(d) { return d['totalemigrant'] })),
+                                parseInt(response1.data.filter(d => d.country == 'China').map(function(d) { return d['totalemigrant'] })),
+                                parseInt(response1.data.filter(d => d.country == 'Colombia').map(function(d) { return d['totalemigrant'] })),
+                                parseInt(response1.data.filter(d => d.country == 'Germany').map(function(d) { return d['totalemigrant'] })),
+                                parseInt(response1.data.filter(d => d.country == 'USA').map(function(d) { return d['totalemigrant'] }))
+                            ]
+                        }, {
+                            name: 'emigrant man',
+                            data: [parseInt(response1.data.filter(d => d.country == 'Spain').map(function(d) { return d['emigrantman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'China').map(function(d) { return d['emigrantman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'Colombia').map(function(d) { return d['emigrantman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'Germany').map(function(d) { return d['emigrantman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'USA').map(function(d) { return d['emigrantman'] }))
+                            ]
+                        }, {
+                            name: 'emigrant woman',
+                            data: [parseInt(response1.data.filter(d => d.country == 'Spain').map(function(d) { return d['emigrantwoman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'China').map(function(d) { return d['emigrantwoman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'Colombia').map(function(d) { return d['emigrantwoman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'Germany').map(function(d) { return d['emigrantwoman'] })),
+                                parseInt(response1.data.filter(d => d.country == 'USA').map(function(d) { return d['emigrantwoman'] }))
+                            ]
+                        }]
+                    });
+                })
+
+            })
         }
-    },
-    series: [{
-                                    name: 'Tourists Departure',
-                                    data: [parseInt(response.data.filter(d => d.country == 'Spain').map(function(d) { return d["touristDeparture"] })),
-                                        parseInt(response.data.filter(d => d.country == 'China').map(function(d) { return d["touristDeparture"] })),
-                                        parseInt(response.data.filter(d => d.country == 'Colombia').map(function(d) { return d["touristDeparture"] })),
-                                        parseInt(response.data.filter(d => d.country == 'Germany').map(function(d) { return d["touristDeparture"] })),
-                                        parseInt(response.data.filter(d => d.country == 'USA').map(function(d) { return d["touristDeparture"] }))
-
-                                    ]
-                                }, {
-                                    name: 'Income Tourist',
-                                    data: [parseInt(response.data.filter(d => d.country == 'Spain').map(function(d) { return d['incomeTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'China').map(function(d) { return d['incomeTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'Colombia').map(function(d) { return d['incomeTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'Germany').map(function(d) { return d['incomeTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'USA').map(function(d) { return d['incomeTourist'] }))
-                                    ]
-                                }, {
-                                    name: 'arrival Tourist',
-                                    data: [parseInt(response.data.filter(d => d.country == 'Spain').map(function(d) { return d['arrivalTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'China').map(function(d) { return d['arrivalTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'Colombia').map(function(d) { return d['arrivalTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'Germany').map(function(d) { return d['arrivalTourist'] })),
-                                        parseInt(response.data.filter(d => d.country == 'USA').map(function(d) { return d['arrivalTourist'] }))
-                                    ]
-                                }, {
-                                    name: 'total emigrations',
-                                    data: [parseInt(response1.data.filter(d => d.country == 'Spain').map(function(d) { return d['totalemigrant'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'China').map(function(d) { return d['totalemigrant'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'Colombia').map(function(d) { return d['totalemigrant'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'Germany').map(function(d) { return d['totalemigrant'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'USA').map(function(d) { return d['totalemigrant'] }))
-                                    ]
-                                }, {
-                                    name: 'emigrant man',
-                                    data: [parseInt(response1.data.filter(d => d.country == 'Spain').map(function(d) { return d['emigrantman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'China').map(function(d) { return d['emigrantman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'Colombia').map(function(d) { return d['emigrantman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'Germany').map(function(d) { return d['emigrantman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'USA').map(function(d) { return d['emigrantman'] }))
-                                    ]
-                                }, {
-                                    name: 'emigrant woman',
-                                    data: [parseInt(response1.data.filter(d => d.country == 'Spain').map(function(d) { return d['emigrantwoman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'China').map(function(d) { return d['emigrantwoman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'Colombia').map(function(d) { return d['emigrantwoman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'Germany').map(function(d) { return d['emigrantwoman'] })),
-                                        parseInt(response1.data.filter(d => d.country == 'USA').map(function(d) { return d['emigrantwoman'] }))
-                                    ]
-                                }]
-});
-                        })
-
-                    })
-}
-                ]);
+    ]);
