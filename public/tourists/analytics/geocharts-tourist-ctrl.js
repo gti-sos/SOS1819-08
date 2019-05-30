70ce2bf3e54f85e66ab7f8c12b475ddc1a352556
sos1819mdls <lolimarilopez@hotmail.es>
2019-05-30 12:16:12 +0000

    quito codigo geomaps

--- public/tourists/analytics/geocharts-tourist-ctrl.js
+++ public/tourists/analytics/geocharts-tourist-ctrl.js
@@ -13,7 +13,7 @@ angular
         'packages': ['geochart'],
         // Note: you will need to get a mapsApiKey for your project.
         // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-        'mapsApiKey': 'AIzaSyBHY1cV7da92cIQ7IHqGsQdWQxDzxMQjCg'
+        'mapsApiKey': ''
       });
                 google.charts.setOnLoadCallback(drawMarkersMap);
                 /*console.log("variables :" + response.data.filter(d => d.province == "huelva" && d.year == 2016).map(d => { return d.year }));*/
@@ -38,3 +38,4 @@ angular
       }
   })
        }]);
+//AIzaSyBHY1cV7da92cIQ7IHqGsQdWQxDzxMQjCg
