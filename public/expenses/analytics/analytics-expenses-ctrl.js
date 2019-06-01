357623c12c6f94f4caeeaf4ddf7470e82c6b3209
sos1819fgm <felgonmar@alum.us.es>
2019-06-01 23:20:51 +0000

    visual

--- public/expenses/analytics/analytics-expenses-ctrl.js
+++ public/expenses/analytics/analytics-expenses-ctrl.js
@@ -97,60 +97,84 @@ console.log(data1)
       
       
       //PLOTTABLE
-      
-      function generateData() {
+       
   var data=[];
-  var val= 500;
+  var symbolSize = 10;
+  
   for (var i in response.data) {
-   var dat={
+        
+  var  dat2={
                         name: response.data.map(function(d) { return d["country"] })[i]+" "+ response.data.map(function(d) { return d["year"] })[i],
                         expenses: response.data.map(function(d) { return d["countryExpense"] })[i],
-                        epc: response.data.map(function(d) { return d["expensePerCapita"] })[i],
-                        value:  + data[data.length - 1].value + Math.random()
-   }
-                        data.push(dat);
+   };
+                        data.push(dat2);
                     }
+                    
   
-  return data;
-}
 
-console.log("Datos para plottable: "+JSON.stringify(generateData()));
+console.log("Datos para plottable: "+JSON.stringify(data));
+    var xScale = new Plottable.Scales.Category();
+    var yScale = new Plottable.Scales.Linear();
+    var xAxis = new Plottable.Axes.Category(xScale, "bottom");
+    var yAxis = new Plottable.Axes.Numeric(yScale, "left");
 
-function generatePlotGroup(xScale, yScale) {
-  var linePlot = new Plottable.Plots.Line()
-    .addDataset(new Plottable.Dataset(generateData()))
-    .addDataset(new Plottable.Dataset(generateData()))
-    .x(function(d) { return d.name; }, xScale)
-    .y(function(d) { return d.value; }, yScale)
-    .attr("opacity", 0.9);
+    var linePlot = new Plottable.Plots.Line()
+  .addDataset(new Plottable.Dataset(data))
+  .x(function(d) { return d.name; }, xScale)
+  .y(function(d) { return d.expenses; }, yScale)
+  .attr("stroke-width", 3)
+  .attr("stroke", "black")
+  .addDataset(new Plottable.Dataset(data));
 
-  var datasetForFocusPoint = new Plottable.Dataset();
+    var scatterPlot = new Plottable.Plots.Scatter()
+  .addDataset(new Plottable.Dataset(data))
+  .x(function(d) { return d.name; }, xScale)
+  .y(function(d) { return d.expenses; }, yScale)
+  .attr("opacity", 1)
+  .attr("stroke-width", 3)
+  .attr("stroke", "black")
+  .size(symbolSize)
+  .addDataset(new Plottable.Dataset(data));
+  
+  
+  var bandPlot = new Plottable.Plots.Rectangle()
+  .x(function(d) { return d.name; }, xScale)
+  .y(0)
+  .y2(function() { return bandPlot.height(); })
+  .attr("fill", "white")
+  .attr("opacity", 0.3)
+  .addDataset(new Plottable.Dataset(data));
 
-  var selectedPoint = new Plottable.Plots.Scatter()
-    .x(function(d) { return d.date; }, xScale)
-    .y(function(d) { return d.value; }, yScale)
-    .size(10)
-    .attr("opacity", 1)
-    .addDataset(datasetForFocusPoint);
-
-  var selectedPointHighlight = new Plottable.Plots.Scatter()
-    .x(function(d) { return d.date; }, xScale)
-    .y(function(d) { return d.value; }, yScale)
-    .size(20)
-    .attr("opacity", 0.25)
-    .addDataset(datasetForFocusPoint);
-
-  var guideline = new Plottable.Components.GuideLineLayer(
-    Plottable.Components.GuideLineLayer.ORIENTATION_VERTICAL
-  ).scale(xScale);
-
-  return new Plottable.Components.Group([linePlot, guideline, selectedPoint, selectedPointHighlight]);
-}
-
-
-
-
+var interaction = new Plottable.Interactions.Pointer();
+interaction.onPointerMove(function(point) {
+  bandPlot.entities().forEach(function(entity) {
+    entity.selection.attr("fill", "white");
+  });
+  var nearestEntity = bandPlot.entityNearest(point);
+  nearestEntity.selection.attr("fill", "#7cb5ec");
+  scatterPlot.size(function(datum) {
+    return datum.ages === nearestEntity.datum.name ? symbolSize * 2 : symbolSize;
+  });
+})
+interaction.onPointerExit(function() {
+  bandPlot.entities().forEach(function(entity) {
+    entity.selection.attr("fill", "white");
+  });
+  scatterPlot.size(symbolSize);
+});
+interaction.attachTo(bandPlot);
    
+
+var plots = new Plottable.Components.Group([bandPlot, linePlot, scatterPlot]);
+
+var table = new Plottable.Components.Table([
+  [yAxis, plots],
+  [null,  xAxis]
+]);
+
+table.renderTo("svg#example");
+
+
 
      });      
              
