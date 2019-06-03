c1f6ca743846b472b01fbe9462f90f0ddd9bccb4
sos1819cjn <crisjinu@hotmail.com>
2019-06-03 11:13:47 +0000

    integracion

--- public/emigration/analytics/vis.js
+++ public/emigration/analytics/vis.js
@@ -5,19 +5,15 @@ angular
         var API = "/api/v1/emigrations-by-countries";
  var container = document.getElementById('visualization');
   var items = [
-    {x: '2014-06-11', y: 10},
-    {x: '2014-06-12', y: 25},
-    {x: '2014-06-13', y: 30},
-    {x: '2014-06-14', y: 10},
-    {x: '2014-06-15', y: 15},
-    {x: '2014-06-16', y: 30}
+    {x: 'Sapin', y: 10},
+    {x: 'USA', y: 25},
   ];
 
   var dataset = new vis.DataSet(items);
 var options = {
     width:  '100%',
     height: '400px',
-    style: 'surface'
+    style: 'bar'
 };
   var graph2d = new vis.Graph2d(container, dataset, options);
     
