5c2a469f8830244f8074b57036c9ffcd2bf76f4b
sos1819cjn <crisjinu@hotmail.com>
2019-06-04 11:33:59 +0000

    integracion

--- public/emigration/analytics/ejschart.js
+++ public/emigration/analytics/ejschart.js
@@ -15,29 +15,54 @@ var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	zoomEnabled: true,
 	title:{
-		text: "Real Estate Rates"
+		text: "Emigrations by countries"
 	},
 	axisX: {
-		title:"Area (in sq. ft)",
+		title:"EmigranMan",
 		minimum: 790,
 		maximum: 6000000
 	},
 	axisY:{
-		title: "Price (in USD)",
+		title: "EmigrantWomanPrice (in USD)",
 		valueFormatString: "$#,##0k"
 	},
 	data: [{
 		type: "scatter",
-		toolTipContent: "<b>Area: </b>{x} sq.ft<br/><b>Price: </b>${y}k",
+		toolTipContent: "<b>USA: Emigrantman: </b>{x} sq.ft<br/><b>Emigrantwoman: </b>${y}k",
 		dataPoints: [
-			{ x: response.data[0].emigrantman, y: response.data[0].emigrantwoman },
-			{ x: response.data[1].emigrantman, y: response.data[1].emigrantwoman },
-			{ x: response.data[2].emigrantman, y: response.data[2].emigrantwoman },
-			{ x: response.data[3].emigrantman, y: response.data[3].emigrantwoman },
-			{ x: response.data[4].emigrantman, y: response.data[4].emigrantwoman },
+			{ x: response.data[0].emigrantman, y: response.data[0].emigrantwoman }
+		]
+	},{
+		type: "scatter",
+		toolTipContent: "<b>China: Emigrantman: </b>{x} sq.ft<br/><b>Emigrantwoman: </b>${y}k",
+		dataPoints: [
+			{ x: response.data[1].emigrantman, y: response.data[1].emigrantwoman }
+		]
+	},{
+		type: "scatter",
+		toolTipContent: "<b>Spain: Emigrantman: </b>{x} sq.ft<br/><b>Emigrantwoman: </b>${y}k",
+		dataPoints: [
+			{ x: response.data[2].emigrantman, y: response.data[2].emigrantwoman }
+		]
+	},{
+		type: "scatter",
+		toolTipContent: "<b>Germany: Emigrantman: </b>{x} sq.ft<br/><b>Emigrantwoman: </b>${y}k",
+		dataPoints: [
+			{ x: response.data[3].emigrantman, y: response.data[3].emigrantwoman }
+		]
+	},{
+		type: "scatter",
+		toolTipContent: "<b>Colombia: Emigrantman: </b>{x} sq.ft<br/><b>Emigrantwoman: </b>${y}k",
+		dataPoints: [
+			{ x: response.data[4].emigrantman, y: response.data[4].emigrantwoman }
+		]
+	},{
+		type: "scatter",
+		toolTipContent: "<b>Uruguay: Emigrantman: </b>{x} sq.ft<br/><b>Emigrantwoman: </b>${y}k",
+		dataPoints: [
 			{ x: response.data[5].emigrantman, y: response.data[5].emigrantwoman }
 		]
-	}]
+	},]
 });
 chart.render();
 
