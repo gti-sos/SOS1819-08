3b69aa512bc6d1c177cab6cacdda39b6991181f6
sos1819mdls <lolimarilopez@hotmail.es>
2019-06-04 17:22:24 +0000

    integraciones ok

--- public/tourists/analytics/integracionesApi-ctrl.js
+++ public/tourists/analytics/integracionesApi-ctrl.js
@@ -425,7 +425,7 @@ pieSeries.hiddenState.properties.startAngle = -90;
 })
     
     
- var APIE1 = "https://www.oostende.be/opendataset/view/6/json";
+ var APIE1 = "https://api.openbrewerydb.org/breweries";
  var API = "api/v1/tourists-by-countries";
  
 
@@ -449,7 +449,98 @@ $http.get(API).then(function(response) {
         }
     }).draw();
                     
+var APIE2 = "/proxyMLS";
+ var API = "api/v1/tourists-by-countries";
+ 
 
+$http.get(API).then(function(response) {     
+
+ $http.get(APIE2).then(function(responseE1) {                  
+
+
+anychart.onDocumentReady(function () {
+    // create data set on our data
+    var dataSet = anychart.data.set([
+        ['poblacion oeste europa', parseInt(responseE1.data.filter(d => d.region == 'Europe' && d.subregion == "Western Europe").map(function(d) { return d['population'] }))],
+        ['Colombia salida de turistas', parseInt(response.data.filter(d => d.country == 'Colombia').map(function(d) { return d['touristDeparture'] }))],
+        ['Germany salida de turistas', parseInt(response.data.filter(d => d.country == 'Germany').map(function(d) { return d['touristDeparture'] }))]
+        
+    ]);
+
+    // map data for the first series, take x from the zero column and value from the first column of data set
+    var seriesData = dataSet.mapAs({'x': 0, 'value': 1});
+
+    // create area chart
+    var chart = anychart.area();
+
+    // adding dollar symbols to yAxis labels
+    chart.yAxis().labels().format('{%Value}');
+
+    // turn on chart animation
+    chart.animation(true);
+
+    // axes and scale settings
+    chart.yScale()
+            .minimum(9000000)
+            .maximum(30000);
+    chart.yAxis().title('integracion2');
+    chart.xAxis().labels().padding([5, 5, 0, 5]);
+
+    // chart grids
+    chart.yGrid(true)
+            .xGrid(true);
+
+    // turn on the crosshair
+    var crosshair = chart.crosshair();
+    crosshair.enabled(true)
+            .yStroke(null)
+            .xStroke('#fff')
+            .zIndex(99);
+    crosshair.yLabel(false);
+    crosshair.xLabel(false);
+
+    // set chart title text settings
+    chart.title()
+            .enabled(true)
+            .useHtml(true)
+            .text('AVERAGE MONTHLY HOURS OF SUNSHINE OVER THE YEAR<br/>' +
+                    '<span style="color:#212121; font-size: 13px;">the monthly total of sunhours over the year in Sydney, Australia.</span>')
+            .padding([0, 0, 20, 0]);
+
+    // create first series with mapped data
+    var series = chart.splineArea(seriesData);
+    series.name('Sunhours');
+    series.color('Gold 0.5');
+    series.markers()
+            .enabled(true)
+            .type('circle')
+            .size(4)
+            .stroke('1.5 #fff')
+            .zIndex(100);
+
+    // set chart tooltip and interactivity settings
+    chart.tooltip()
+            .positionMode('chart')
+            .anchor('right-top')
+            .position('right-top')
+            .offsetX(50)
+            .offsetY(50);
+
+    chart.interactivity().hoverMode('by-x');
+
+    // set container id for the chart
+    chart.container('integracionE2');
+
+    // initiate chart drawing
+    chart.draw();
+});
+
+   
+
+
+ })
+
+})
  
 })
 })
