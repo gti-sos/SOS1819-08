be8740a04942a90f6e8287c98bb8776cd88eaf2d
sos1819cjn <crisjinu@hotmail.com>
2019-06-04 08:22:27 +0000

    integracion

--- public/emigration/analytics/apiExterna1.js
+++ public/emigration/analytics/apiExterna1.js
@@ -12,10 +12,7 @@ angular
             $http.get(API).then(function(response) {
                 $http.get(API2).then(function(response1) {
                     console.log("Data received: "+JSON.stringify(response1.data));
-                    $scope.coin=response1.data;
-                    console.log($scope.coin);
-                    $scope.dataResponse=$scope.coin;
-                    
+                    JSON.stringify(response1.data)
                     
                     
                     Highcharts.chart('container', {
