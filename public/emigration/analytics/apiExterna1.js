9472662b7376145027ae2c4533c9879f75b772c8
sos1819cjn <crisjinu@hotmail.com>
2019-06-04 08:24:36 +0000

    integracion

--- public/emigration/analytics/apiExterna1.js
+++ public/emigration/analytics/apiExterna1.js
@@ -14,60 +14,6 @@ angular
                     console.log("Data received: "+JSON.stringify(response1.data));
                     JSON.stringify(response1.data)
                     
-                    
-                    Highcharts.chart('container', {
-                        chart: {
-                            type: 'column',
-                            options3d: {
-                                enabled: true,
-                                alpha: 10,
-                                beta: 25,
-                                depth: 70
-                            }
-                        },
-                        title: {
-                            text: '3D chart with null values'
-                        },
-                        subtitle: {
-                            text: 'Notice the difference between a 0 value and a null point'
-                        },
-                        plotOptions: {
-                            column: {
-                                depth: 25
-                            }
-                        },
-                        xAxis: {
-                            categories: [response.data[0].country + " "+ response.data[0].year,
-                            response.data[1].country + " "+ response.data[1].year,
-                            response.data[2].country + " "+ response.data[2].year,
-                            response.data[3].country + " "+ response.data[3].year,
-                            response.data[4].country + " "+ response.data[4].year],
-                            labels: {
-                                skew3d: true,
-                                style: {
-                                    fontSize: '16px'
-                                }
-                            }
-                        },
-                        yAxis: {
-                            title: {
-                                text: null
-                            }
-                        },
-                        series: [{
-                            name: 'Emigrant Man',
-                            data: [parseInt(response.data.filter(d => d.country == response.data[0].country).map(function(d) { return d["totalemigrant"] })),
-                                parseInt(response.data.filter(d => d.country == response.data[1].country).map(function(d) { return d["totalemigrant"] })),
-                                parseInt(response.data.filter(d => d.country == response.data[2].country).map(function(d) { return d["totalemigrant"] })),
-                                parseInt(response.data.filter(d => d.country == response.data[3].country).map(function(d) { return d["totalemigrant"] })),
-                                parseInt(response.data.filter(d => d.country == response.data[4].country).map(function(d) { return d["totalemigrant"] }))
-                        ]
-                    }, {
-                        name: 'monedas',
-                        data: [datos[0].rate, datos[1].rate,
-                        ]
-                    }]
-                    });
                 })
 
             })
