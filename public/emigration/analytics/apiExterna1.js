d3b9c0a99ff6be93559a905f5d7ecb3b876f3b5d
sos1819cjn <crisjinu@hotmail.com>
2019-06-04 08:27:22 +0000

    integracion

--- public/emigration/analytics/apiExterna1.js
+++ public/emigration/analytics/apiExterna1.js
@@ -12,7 +12,7 @@ angular
             $http.get(API).then(function(response) {
                 $http.get(API2).then(function(response1) {
                     console.log("Data received: "+JSON.stringify(response1.data));
-                    JSON.stringify(response1.data)
+                     $scope.data = JSON.stringify(response1.data,null,2);
                     
                 })
 
